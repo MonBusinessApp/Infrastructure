dd1aa08c029958735d664ede319adc6f4001e965
Felix Diez <diez.felix@gmail.com>
2023-08-20 17:14:52 +0200

    add terraform boostrap

---
+++ deployment/terraform/local/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">=1.0"
+    }
+  }
+}
+
+
+provider "flux" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+  git = {
+    url = "ssh://git@github.com/MonBusinessApp/Infrastructure.git"
+    ssh = {
+      username    = "git"
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
+}
+
+resource "flux_bootstrap_git" "this" {
+  path = "clusters/local"
+}
